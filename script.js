8682f7c294d1c361aff871df700a71a1d3d46896
Emma-Jonna <johanna.emma.pihl@gmail.com>
2022-11-29 11:10:04 +0100

    fixed hint button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const body = document.querySelector('body');
 const choosingTable = document.querySelector('.choose-multiplication-table');
+const hint = document.querySelector('.hint button');
+const hintText = document.querySelector('.hint p');
 
 const multiplicationTable = [];
 const multiplicationTableAnswers = [];
@@ -12,14 +14,29 @@ let baseNumber = 1;
 
 const getMultiplyTable = () => {};
 const multiplyByTable = () => {};
+const getHintComment = () => {};
+const getAnswerComments = () => {};
+
+hint.addEventListener('click', () => {
+  if (hintText.classList.contains('hidden')) {
+    hintText.classList.remove('hidden');
+  } else {
+    hintText.classList.add('hidden');
+  }
+});
 
 // Writes out all multiplicationtables to the user
 for (let i = 0; i <= 9; i++) {
-  const chooseTableContainer = document.createElement('div');
+  const chooseTableContainer = document.createElement('button');
   const chooseTable = document.createElement('p');
+  chooseTableContainer.classList.add('.multiplication-table');
 
   chooseTableContainer.appendChild(chooseTable);
   choosingTable.appendChild(chooseTableContainer);
+
+  chooseTableContainer.addEventListener('click', () => {
+    console.log(chooseTable.textContent);
+  });
 
   chooseTable.textContent = multiplyBy++;
 }
