e5638f45c8cf1622ae5644d81d1860e6de2114a1
Emma-Jonna <johanna.emma.pihl@gmail.com>
2022-11-29 10:18:16 +0100

    added multiplication tables

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-const multiplicationTable = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+const multiplicationTable = [];
+
+const multiplicationTableAnswers = [];
 
 const hintComments = ["I don't actually know the answer"];
 
 const answerComments = ['WRONG!'];
+
+const getMultiplyTable = () => {};
+const multiplyByTable = () => {};
+
+let multiplyBy = 1;
+
+let baseNumber = 3;
+
+for (let i = 0; i <= 9; i++) {
+  const multiplication = `${baseNumber} x ${multiplyBy}`;
+  const answer = baseNumber * multiplyBy;
+
+  multiplicationTableAnswers.push(answer);
+
+  multiplicationTable.push(multiplication, answer);
+
+  multiplyBy++;
+}
+
+for (let i = 0; i <= 9; i++) {}
+
+multiplicationTable.forEach((element) => {
+  console.log(element);
+});
