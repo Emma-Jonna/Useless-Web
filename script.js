be6575901f3c89fd0e1da3475b61b5e9de8d17fd
Emma-Jonna <johanna.emma.pihl@gmail.com>
2022-11-29 10:41:05 +0100

    can now print numbers on screen

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
+const body = document.querySelector('body');
+const choosingTable = document.querySelector('.choose-multiplication-table');
+
 const multiplicationTable = [];
-
 const multiplicationTableAnswers = [];
-
 const hintComments = ["I don't actually know the answer"];
-
 const answerComments = ['WRONG!'];
+
+let multiplyBy = 1;
+
+let baseNumber = 1;
 
 const getMultiplyTable = () => {};
 const multiplyByTable = () => {};
 
-let multiplyBy = 1;
+// Writes out all multiplicationtables to the user
+for (let i = 0; i <= 9; i++) {
+  const chooseTableContainer = document.createElement('div');
+  const chooseTable = document.createElement('p');
 
-let baseNumber = 3;
+  chooseTableContainer.appendChild(chooseTable);
+  choosingTable.appendChild(chooseTableContainer);
 
+  chooseTable.textContent = multiplyBy++;
+}
+
+// Puts the questions and answers depending on wich the user chosed
 for (let i = 0; i <= 9; i++) {
   const multiplication = `${baseNumber} x ${multiplyBy}`;
   const answer = baseNumber * multiplyBy;
 
   multiplicationTableAnswers.push(answer);
 
-  multiplicationTable.push(multiplication, answer);
+  multiplicationTable.push(multiplication);
 
   multiplyBy++;
 }
-
-for (let i = 0; i <= 9; i++) {}
-
-multiplicationTable.forEach((element) => {
-  console.log(element);
-});
